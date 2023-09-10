ec7d36906d4a5c0311541462959ffa2ed367fa08
envy2810 <parvez.mca22@bmsce.ac.in>
2023-09-10 22:18:13 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import Modal from "./Modal";
+const Footer = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <>
+      <footer className="footer">
+        <div class="attribution">
+          Copyright © <a href="https://github.com/envy2810" target="_blank">Parvez Mussarf Hussain & B Dilip</a>
+        </div>
+        <button className="rules" onClick={toggle}>
+          Rules
+        </button>
+      </footer>
+      {modal ? <Modal toggle={toggle} /> : null}
+    </>
+  );
+};
+
+export default Footer;
